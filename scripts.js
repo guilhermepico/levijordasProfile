3a7348987b94c6ab65b271f367fa77eec54bfe81
guilhermepico <gui.pico@outlook.com>
2025-02-06 19:38:05 -0300

    adjusting gb company popup

--- scripts.js
+++ scripts.js
@@ -1,4 +1,24 @@
+// Referências aos elementos
+const openPopupBtn = document.getElementById("openPopupBtn");
+const popup = document.getElementById("popup");
+const closePopupBtn = document.getElementById("closePopupBtn");
 
+// Função para abrir o popup
+openPopupBtn.addEventListener("click", () => {
+    popup.classList.add("show");
+});
+
+// Função para fechar o popup
+closePopupBtn.addEventListener("click", () => {
+    popup.classList.remove("show");
+});
+
+// Fechar o popup se o fundo (fora do conteúdo) for clicado
+popup.addEventListener("click", (e) => {
+    if (e.target === popup) {
+        popup.classList.remove("show");
+    }
+});
 
     
 
